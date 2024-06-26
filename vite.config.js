eaf3c5f660e63c7491d6861f90562f4e5765aa3e
e-duardo-lucas <el035627@gmail.com>
2024-06-26 09:12:31 -0600

    primer  cambio

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/LukiControl/',  // Cambia 'nombre-del-repo' por el nombre de tu repositorio
   plugins: [
     vue(),
   ],
